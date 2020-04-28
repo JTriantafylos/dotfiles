79cc0c476e9f9cd0e72ad702b26917f2e6741531
James Triantafylos <JTriantafylos@gmail.com>
2020-04-28 00:42:57 -0400

    Updated install script

--- install.sh
+++ install.sh
@@ -16,11 +16,16 @@ ln -sv "${src_dir}/zsh/zlogin" $zsh_dir/.zlogin
 # X11
 X11_dir=$HOME
 ln -sv "${src_dir}/X11/xinitrc" $X11_dir/.xinitrc
+ln -sv "${src_dir}/X11/Xresources" $X11_dir/.Xresources
 
 # neovim
 nvim_dir=$HOME/.config/nvim
 ln -sv "${src_dir}/nvim/init.vim" $nvim_dir/init.vim
 ln -sv "${src_dir}/nvim/coc-settings.json" $nvim_dir/coc-settings.json
+
+# picom
+picom_dir=$HOME/.config/picom
+ln -sv "${src_dir}/picom/picom.conf" $picom_dir/picom.conf
 
 # wallpaper
 wallpaper_dir=$HOME
