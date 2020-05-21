9d116ffedaa23b315ea145f0db8c51562d9c8fd6
James Triantafylos <JTriantafylos@gmail.com>
2020-05-20 21:24:13 -0400

    Added tmux config

--- install.sh
+++ install.sh
@@ -32,6 +32,10 @@ ln -sv "${src_dir}/picom/picom.conf" $picom_dir/picom.conf
 starship_dir=$HOME/.config
 ln -sv "${src_dir}/starship/starship.toml" $starship_dir/starship.toml
 
+# tmux
+tmux_dir=$HOME
+ln -sv "${src_dir}/tmux/tmux.conf" $tmux_dir/.tmux.conf
+
 # wallpaper
 wallpaper_dir=$HOME
 ln -sv "${src_dir}/wallpaper/wallpaper.jpg" $wallpaper_dir/.wallpaper.jpg
