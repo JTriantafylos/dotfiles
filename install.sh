ecdd2f7889590ecced0dbf81efbf95adc9f6f4fc
James Triantafylos <JTriantafylos@gmail.com>
2020-09-28 20:54:54 -0400

    Switched back to i3

--- install.sh
+++ install.sh
@@ -8,22 +8,6 @@ i3_dir=$HOME/.config/i3
 ln -sv "${src_dir}/i3/config" $i3_dir/config
 ln -sv "${src_dir}/i3/statusline" $i3_dir/statusline
 
-# xmonad
-xmonad_dir=$HOME/.config/xmonad
-ln -sv "${src_dir}/xmonad/xmonad.hs" $xmonad_dir/xmonad.hs
-
-# xmobar
-xmobar_dir=$HOME/.config/xmobar
-ln -sv "${src_dir}/xmobar/xmobarrc" $xmobar_dir/xmobarrc
-
-# awesome
-awesome_dir=$HOME/.config/awesome
-ln -sv "${src_dir}/awesome/rc.lua" $awesome_dir/rc.lua
-mkdir -v $awesome_dir/themes
-ln -sv "${src_dir}/awesome/themes/default.lua" $awesome_dir/themes/default.lua
-ln -sv "${src_dir}/awesome/themes/assets.lua" $awesome_dir/themes/assets.lua
-ln -sv "${src_dir}/awesome/themes/theme.lua" $awesome_dir/themes/theme.lua
-
 # termite
 termite_dir=$HOME/.config/termite
 ln -sv "${src_dir}/termite/config" $termite_dir/config
