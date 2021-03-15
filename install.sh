15be0e04c2e497f52fc3cad32af08bae64b3963e
James Triantafylos <JTriantafylos@gmail.com>
2021-03-15 15:55:33 -0400

    Force symbolic links to be created

--- install.sh
+++ install.sh
@@ -5,45 +5,45 @@ src_dir=$(dirname $(realpath $0))
 
 # i3
 i3_dir=$HOME/.config/i3
-ln -sv "${src_dir}/i3/config" $i3_dir/config
-ln -sv "${src_dir}/i3/statusline" $i3_dir/statusline
+ln -sfv "${src_dir}/i3/config" $i3_dir/config
+ln -sfv "${src_dir}/i3/statusline" $i3_dir/statusline
 
 # termite
 termite_dir=$HOME/.config/termite
-ln -sv "${src_dir}/termite/config" $termite_dir/config
+ln -sfv "${src_dir}/termite/config" $termite_dir/config
 
 # zsh
 zsh_dir=$HOME
-ln -sv "${src_dir}/zsh/zshrc" $zsh_dir/.zshrc
-ln -sv "${src_dir}/zsh/zshenv" $zsh_dir/.zshenv
+ln -sfv "${src_dir}/zsh/zshrc" $zsh_dir/.zshrc
+ln -sfv "${src_dir}/zsh/zshenv" $zsh_dir/.zshenv
 
 # X11
 X11_dir=$HOME/.config/X11
-ln -sv "${src_dir}/X11/xinitrc" $X11_dir/xinitrc
-ln -sv "${src_dir}/X11/Xresources" $X11_dir/Xresources
-ln -sv "${src_dir}/X11/xprofile" $HOME/.xprofile
+ln -sfv "${src_dir}/X11/xinitrc" $X11_dir/xinitrc
+ln -sfv "${src_dir}/X11/Xresources" $X11_dir/Xresources
+ln -sfv "${src_dir}/X11/xprofile" $HOME/.xprofile
 
 # neovim
 nvim_dir=$HOME/.config/nvim
-ln -sv "${src_dir}/nvim/init.vim" $nvim_dir/init.vim
-ln -sv "${src_dir}/nvim/coc-settings.json" $nvim_dir/coc-settings.json
-ln -sv ${src_dir}/nvim/modules/* $nvim_dir/modules/
+ln -sfv "${src_dir}/nvim/init.vim" $nvim_dir/init.vim
+ln -sfv "${src_dir}/nvim/coc-settings.json" $nvim_dir/coc-settings.json
+ln -sfv ${src_dir}/nvim/modules/* $nvim_dir/modules/
 
 # starship
 starship_dir=$HOME/.config
-ln -sv "${src_dir}/starship/starship.toml" $starship_dir/starship.toml
+ln -sfv "${src_dir}/starship/starship.toml" $starship_dir/starship.toml
 
 # zathura
 zathura_dir=$HOME/.config/zathura
-ln -sv "${src_dir}/zathura/zathurarc" $zathura_dir/zathurarc
+ln -sfv "${src_dir}/zathura/zathurarc" $zathura_dir/zathurarc
 
 # wallpaper
 wallpaper_dir=$HOME/.config
-ln -sv "${src_dir}/wallpaper/wallpaper2.jpg" $wallpaper_dir/wallpaper.jpg
+ln -sfv "${src_dir}/wallpaper/wallpaper2.jpg" $wallpaper_dir/wallpaper.jpg
 
 #scripts
 scripts_dir=$HOME/.local/bin
-ln -sv "${src_dir}/scripts/dmenu-fzf" $scripts_dir/dmenu-fzf
-ln -sv "${src_dir}/scripts/newterm" $scripts_dir/newterm
-ln -sv "${src_dir}/scripts/carleton-vpn-connect" $scripts_dir/carleton-vpn-connect
-ln -sv "${src_dir}/scripts/carleton-ssh-COMP3000B" $scripts_dir/carleton-ssh-COMP3000B
+ln -sfv "${src_dir}/scripts/dmenu-fzf" $scripts_dir/dmenu-fzf
+ln -sfv "${src_dir}/scripts/newterm" $scripts_dir/newterm
+ln -sfv "${src_dir}/scripts/carleton-vpn-connect" $scripts_dir/carleton-vpn-connect
+ln -sfv "${src_dir}/scripts/carleton-ssh-COMP3000B" $scripts_dir/carleton-ssh-COMP3000B
--- install_home-server.sh
+++ install_home-server.sh
@@ -5,11 +5,11 @@ src_dir=$(dirname $(realpath $0))
 
 # zsh
 zsh_dir=$HOME
-ln -sv "${src_dir}/zsh/zshrc" $zsh_dir/.zshrc
-ln -sv "${src_dir}/zsh/zshenv" $zsh_dir/.zshenv
+ln -sfv "${src_dir}/zsh/zshrc" $zsh_dir/.zshrc
+ln -sfv "${src_dir}/zsh/zshenv" $zsh_dir/.zshenv
 
 # neovim
 nvim_dir=$HOME/.config/nvim
-ln -sv "${src_dir}/nvim/init.vim" $nvim_dir/init.vim
-ln -sv "${src_dir}/nvim/coc-settings.json" $nvim_dir/coc-settings.json
-ln -sv ${src_dir}/nvim/modules/* $nvim_dir/modules/
+ln -sfv "${src_dir}/nvim/init.vim" $nvim_dir/init.vim
+ln -sfv "${src_dir}/nvim/coc-settings.json" $nvim_dir/coc-settings.json
+ln -sfv ${src_dir}/nvim/modules/* $nvim_dir/modules/
