e6a380ec7a5eb3538eb549ace7842ce264657dcc
James Triantafylos <jtriantafylos@protonmail.com>
2022-02-18 15:14:35 -0500

    Add filename back to lualine

--- nvim/.config/nvim/lua/plugin-conf/lualine.lua
+++ nvim/.config/nvim/lua/plugin-conf/lualine.lua
@@ -3,6 +3,7 @@ local gps = require('nvim-gps')
 require('lualine').setup {
     sections = {
         lualine_c = {
+        { 'filename' },
         { gps.get_location, cond = gps.is_available },
         }
     }
