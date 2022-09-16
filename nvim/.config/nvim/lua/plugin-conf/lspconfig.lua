2ae301e3273a5a31e58c6f6fdd24fd648a5eec03
James Triantafylos <jtriantafylos@protonmail.com>
2022-09-16 10:43:53 -0400

    Update LSP keybinds

--- nvim/.config/nvim/lua/plugin-conf/lspconfig.lua
+++ nvim/.config/nvim/lua/plugin-conf/lspconfig.lua
@@ -1,26 +1,27 @@
 local lspconfig = require('lspconfig')
 
 -- Keybinds
-local default_opts = { noremap = true, silent = true }
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
+-- Each LSP server setup method must set on_attach equal to this function
 local on_attach = function(_, bufnr)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd',         '<cmd>lua require(\'telescope.builtin\').lsp_definitions()<CR>', default_opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K',          '<cmd>lua vim.lsp.buf.hover()<CR>', default_opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi',         '<cmd>lua require(\'telescope.builtin\').lsp_implementations()<CR>', default_opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>',      '<cmd>lua vim.lsp.buf.signature_help()<CR>', default_opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', default_opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr',         '<cmd>lua require(\'telescope.builtin\').lsp_references()<CR>', default_opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>s',  '<cmd>lua require(\'telescope.builtin\').lsp_document_symbols()<CR>', default_opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gh',         '<cmd>ClangdSwitchSourceHeader<CR>', default_opts)
+    local bufopts = { buffer=bufnr }
+    vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions(), bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover(), bufopts)
+    vim.keymap.set('n', 'gi', require('telescope.builtin').lsp_implementations(), bufopts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help(), bufopts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename(), bufopts)
+    vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references(), bufopts)
+    vim.keymap.set('n', '<leader>s', require('telescope.builtin').lsp_document_symbols(), bufopts)
+    vim.keymap.set('n', 'gh', '<cmd>ClangdSwitchSourceHeader<CR>', bufopts)
 end
 
 -- Initialize cmp-nvim-lsp capabilities
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- Setup clangd LSP server
-require("clangd_extensions").setup {
+require('clangd_extensions').setup{
     server = {
         on_attach = on_attach,
         capabilities = capabilities,
@@ -28,13 +29,21 @@ require("clangd_extensions").setup {
 }
 
 -- Setup jedi-language-server
-lspconfig.jedi_language_server.setup{}
+lspconfig.jedi_language_server.setup{
+    on_attach = on_attach
+}
 
 -- Setup typescript-language-server
-lspconfig.tsserver.setup{}
+lspconfig.tsserver.setup{
+    on_attach = on_attach
+}
+
 
 -- Setup ESLint LSP server
-lspconfig.eslint.setup{}
+lspconfig.eslint.setup{
+    on_attach = on_attach
+}
+
 
 -- Setup Lua runetime path
 local runtime_path = vim.split(package.path, ';')
@@ -43,26 +52,27 @@ table.insert(runtime_path, "lua/?/init.lua")
 
 -- Setup Lua-Language-Server
 lspconfig.sumneko_lua.setup {
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-        -- Setup your lua path
-        path = runtime_path,
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
+    on_attach = on_attach,
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+                -- Setup your lua path
+                path = runtime_path,
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {'vim'},
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
     },
-  },
 }
