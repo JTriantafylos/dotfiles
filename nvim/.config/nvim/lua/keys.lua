8467b2c738ebcad08f2046e775ca25ffcc6f45b8
James Triantafylos <jtriantafylos@protonmail.com>
2022-09-16 21:12:34 -0400

    Update keybindings to use new keymap API

--- nvim/.config/nvim/lua/keys.lua
+++ nvim/.config/nvim/lua/keys.lua
@@ -1,20 +1,17 @@
--- TODO: Switch keybinds over to vim.keymap api when it becomes availale
-local default_opts = { noremap = true, silent = true }
-
 -- Use space as the leader key
-vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
+vim.keymap.set('', '<Space>', '<Nop>')
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Shortcut to write file to disk
-vim.api.nvim_set_keymap('n', 'ww', ':write<CR>', default_opts)
+vim.keymap.set('n', 'ww', ':write<CR>')
 
 -- Allow indent/de-indent multiple times in a row
-vim.api.nvim_set_keymap('v', '<', '<gv', default_opts)
-vim.api.nvim_set_keymap('v', '>', '>gv', default_opts)
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
 
 -- Paste over currently selected text without yanking it
-vim.api.nvim_set_keymap('v', 'p', '"_dP', default_opts)
+vim.keymap.set('v', 'p', '"_dP')
 
 -- Cancel search highlighting
-vim.api.nvim_set_keymap('n', '<ESC>', ':nohlsearch<CR>', default_opts)
+vim.keymap.set('n', '<ESC>', ':nohlsearch<CR>')
