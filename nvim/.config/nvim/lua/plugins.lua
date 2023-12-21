5bd7777b80c654ebe1cfa59cb45c139fd6c3872a
James Triantafylos <jtriantafylos@protonmail.com>
2023-12-21 01:57:39 -0500

    Move more simple plugin configuration to plugins.lua

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -84,7 +84,19 @@ local lazy_plugin_spec = {
     {
         'nvim-lualine/lualine.nvim',
         dependencies = {
+            'SmiteshP/nvim-navic', -- Codecrumbs
             'nvim-tree/nvim-web-devicons', -- Icons
+        },
+        opts = {
+            sections = {
+                lualine_c = {
+                    { 'filename' },
+                    {
+                        function() return require('nvim-navic').get_location() end,
+                        cond = function() return require('nvim-navic').is_available() end,
+                    },
+                },
+            },
         },
     },
 
@@ -93,6 +105,45 @@ local lazy_plugin_spec = {
         'SmiteshP/nvim-navic',
         dependencies = {
             'neovim/nvim-lspconfig',
+        },
+        opts = {
+            icons = {
+                File          = '󰈙 ',
+                Module        = ' ',
+                Namespace     = '󰌗 ',
+                Package       = ' ',
+                Class         = '󰌗 ',
+                Method        = '󰆧 ',
+                Property      = ' ',
+                Field         = ' ',
+                Constructor   = ' ',
+                Enum          = '󰕘',
+                Interface     = '󰕘',
+                Function      = '󰊕 ',
+                Variable      = '󰆧 ',
+                Constant      = '󰏿 ',
+                String        = '󰀬 ',
+                Number        = '󰎠 ',
+                Boolean       = '◩ ',
+                Array         = '󰅪 ',
+                Object        = '󰅩 ',
+                Key           = '󰌋 ',
+                Null          = '󰟢 ',
+                EnumMember    = ' ',
+                Struct        = '󰌗 ',
+                Event         = ' ',
+                Operator      = '󰆕 ',
+                TypeParameter = '󰊄 ',
+            },
+            lsp = {
+                auto_attach = true,
+                preference = nil,
+            },
+            highlight = false,
+            separator = ' > ',
+            depth_limit = 0,
+            depth_limit_indicator = '..',
+            safe_output = true,
         },
     },
 
