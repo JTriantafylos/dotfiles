167a00f7f8e5ce436a9693bd9b8b7a2ff8ec9345
James Triantafylos <jtriantafylos@protonmail.com>
2025-01-28 23:08:54 -0500

    Use silent mappings where applicable

--- nvim/.config/nvim/lua/keys.lua
+++ nvim/.config/nvim/lua/keys.lua
@@ -4,7 +4,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Shortcut to write file to disk
-vim.keymap.set('n', 'ww', ':write<CR>')
+vim.keymap.set('n', 'ww', ':write<CR>',  { silent = true })
 
 -- Allow indent/de-indent multiple times in a row
 vim.keymap.set('v', '<', '<gv')
@@ -14,8 +14,8 @@ vim.keymap.set('v', '>', '>gv')
 vim.keymap.set('v', 'p', '"_dP')
 
 -- Cancel search highlighting
-vim.keymap.set('n', '<ESC>', ':nohlsearch<CR>')
+vim.keymap.set('n', '<ESC>', ':nohlsearch<CR>',  { silent = true })
 
 -- Shortcuts to previous and next buffers
-vim.keymap.set('n', '[b', ':bprev<CR>')
-vim.keymap.set('n', ']b', ':bnext<CR>')
+vim.keymap.set('n', '[b', ':bprev<CR>',  { silent = true })
+vim.keymap.set('n', ']b', ':bnext<CR>',  { silent = true })
