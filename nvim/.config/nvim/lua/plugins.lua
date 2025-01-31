15e46683caa98f09612c9db249d5b2209332de5e
James Triantafylos <jtriantafylos@protonmail.com>
2025-01-30 23:07:23 -0500

    Use the recommended noice.nvim settings (from the Github page)

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -71,7 +71,17 @@ local lazy_plugin_spec = {
         dependencies = {
             'MunifTanjim/nui.nvim',
             'rcarriga/nvim-notify',
-        }
+        },
+        opts = {
+            lsp = {
+                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                override = {
+                    ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+                    ['vim.lsp.util.stylize_markdown'] = true,
+                    ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+                },
+            },
+        },
     },
 
     -- Statusline
