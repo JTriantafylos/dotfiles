2cb67124d8792133de4fdbec1e8fbdca3762b55c
James Triantafylos <jtriantafylos@protonmail.com>
2025-04-03 00:48:41 -0400

    Integrate mini.statusline

---
+++ nvim/.config/nvim/lua/plugins/mini-statusline.lua
@@ -0,0 +1,10 @@
+return {
+    'echasnovski/mini.statusline',
+    dependencies = {
+        -- Icon support
+        { 'echasnovski/mini.icons', config = true, },
+        -- Git status and diff support
+        { 'lewis6991/gitsigns.nvim', config = true, },
+    },
+    config = true,
+}
