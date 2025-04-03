7599b3d00b4d21973c900620fc93ce475b7b75ba
James Triantafylos <jtriantafylos@protonmail.com>
2025-04-03 01:17:16 -0400

    Integrate render-markdown.nvim

---
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,11 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = {
+        -- makrdown and markdown_inline treesitter parsers
+        'nvim-treesitter/nvim-treesitter',
+        -- Icon support
+        'echasnovski/mini.icons',
+    },
+    -- Lazy-load once a markdown file is opened
+    ft = 'markdown',
+}
